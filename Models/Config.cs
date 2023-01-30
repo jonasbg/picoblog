ef135b5312c30fdf8e9783b39de31d3dff27e467
Jonasbg <jonasbg@gmail.com>
2023-01-30 07:39:58 +0100

    Synology Sizes
    Set synology sizes

--- Models/Config.cs
+++ Models/Config.cs
@@ -4,4 +4,15 @@ public static class Config{
   public static bool Synology => Environment.GetEnvironmentVariable("SYNOLOGY_SUPPORT") == "true";
   public static string DataDir => Environment.GetEnvironmentVariable("DATA_DIR");
   public static string Domain => $"https://{Environment.GetEnvironmentVariable("DOMAIN")}";
+  public static string SynologySize() {
+    var size = Environment.GetEnvironmentVariable("SYNOLOGY_SIZE");
+    var defaultSize = "XL";
+    var allowedSizes = new string[] {"SM", "M", "XL"};
+    if(!allowedSizes.Any(p => p == size.ToUpper()))
+    {
+      Console.WriteLine($"WRONG SYNOLOGY_SIZE SELECTED {size}. DEFAULTING TO {defaultSize}");
+      return $"SYNOPHOTO_THUMB_{size.ToUpper()}.jpg";
+    }
+    return $"SYNOPHOTO_THUMB_{defaultSize}.jpg";
+  }
 }
