8a0b683a03397df13766bb424a9838b546a14ca9
Jonasbg <jonasbg@gmail.com>
2023-03-05 08:51:52 +0100

    Memories on frontpage

--- Controllers/MemoriesController.cs
+++ Controllers/MemoriesController.cs
@@ -11,10 +11,12 @@ public class MemoriesController : Controller
   [Route("[Controller]")]
   public IActionResult Index(){
     var today = DateTime.Now;
+    var lower = today.AddDays(-7).Day > today.Day ? 0 : today.AddDays(-7).Day;
+    var upper = today.AddDays(7).Day < today.Day ? 0 : today.AddDays(7).Day;
     var onThisDay = Cache.Models.Where(
       p => p.Date?.Month == today.Month &&
-      p.Date?.Day <= today.AddDays(7).Day &&
-      p.Date?.Day >= today.AddDays(-7).Day);
-    return Ok(onThisDay);
+      p.Date?.Day <= upper &&
+      p.Date?.Day >= lower);
+    return PartialView("_index.content", onThisDay);
   }
 }
