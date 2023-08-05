3ba49176fd2d2ae86d4ef80bf3b9f40d203049f1
Jonasbg <1508560+jonasbg@users.noreply.github.com>
2023-08-05 21:33:00 +0200

    Create GlobalUsings.cs

---
+++ GlobalUsings.cs
@@ -0,0 +1,12 @@
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authentication.Cookies;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Caching.Memory;
+global using picoblog.Models;
+global using SixLabors.ImageSharp;
+global using SixLabors.ImageSharp.Formats.Jpeg;
+global using SixLabors.ImageSharp.Processing;
+global using System.Diagnostics;
+global using System.Security.Claims;
+global using System.Threading.Channels;
