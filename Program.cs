20ddd11c592d328d1429f3dd3895c9d3f9f274ad
Jonasbg <1508560+jonasbg@users.noreply.github.com>
2023-12-21 09:54:32 +0100

    formatting

--- Program.cs
+++ Program.cs
@@ -22,31 +22,31 @@ builder.Services.AddSingleton<IBackgroundTaskQueue>(ctx =>
 
 if (Config.Password != null)
 {
-  builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-     .AddCookie(options =>
-     {
-       options.Cookie.HttpOnly = true; 
-       options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
-       options.Cookie.SameSite = SameSiteMode.Strict;
-       options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-       options.ExpireTimeSpan = TimeSpan.FromDays(30);
-       options.SlidingExpiration = true;
-       options.Cookie.Name = "Picoblog.AuthCookie"; 
-       options.LoginPath = "/login";
-     });
-  builder.Services.AddControllersWithViews(options =>
-  {
-    options.Filters.Add(new AuthorizeFilter());
-    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-  });
-  builder.Services.AddDataProtection()
-        .PersistKeysToFileSystem(new DirectoryInfo(Config.ConfigDir));
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.Cookie.HttpOnly = true;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+            options.Cookie.SameSite = SameSiteMode.Strict;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.ExpireTimeSpan = TimeSpan.FromDays(30);
+            options.SlidingExpiration = true;
+            options.Cookie.Name = "Picoblog.AuthCookie";
+            options.LoginPath = "/login";
+        });
+    builder.Services.AddControllersWithViews(options =>
+    {
+        options.Filters.Add(new AuthorizeFilter());
+        options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+    });
+    builder.Services.AddDataProtection()
+            .PersistKeysToFileSystem(new DirectoryInfo(Config.ConfigDir));
 }
 else
-  builder.Services.AddControllersWithViews(options =>
-  {
-      options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-  });
+    builder.Services.AddControllersWithViews(options =>
+    {
+        options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+    });
 
 // builder.Services.AddWebOptimizer(pipeline =>
 // {
@@ -58,15 +58,15 @@ var app = builder.Build();
 // app.UseImageSharp();
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
-    {
-        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-    });
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 if (Config.Password != null)
 {
-  app.UseCookiePolicy();
-  app.UseAuthentication();
-  app.UseAuthorization();
+    app.UseCookiePolicy();
+    app.UseAuthentication();
+    app.UseAuthorization();
 }
 
 //app.UseWebOptimizer();
@@ -77,14 +77,15 @@ app.UseRouting();
 
 var supportedCultures = new[]
 {
-   new CultureInfo("nb-NO"),
-   new CultureInfo("en-GB"),
+    new CultureInfo("nb-NO"),
+    new CultureInfo("en-GB"),
 };
 
-app.UseRequestLocalization(new RequestLocalizationOptions{
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
     DefaultRequestCulture = new RequestCulture("nb-NO"),
-    SupportedCultures=supportedCultures,
-    SupportedUICultures=supportedCultures
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
 });
 
 app.MapControllerRoute(
